35ef1c083c91716bc6430a095091e8d2bcac8672
zhp <354416479@qq.com>
2018-10-18 18:03:23 +0800

    a test

--- controllers/hello.js
+++ controllers/hello.js
@@ -3,8 +3,11 @@ var fn_hello = async (ctx, next) => {
   ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 };
 
-
+var fn_goodbye = async (ctx, next) => {
+  ctx.response.body = `<h1>GoodBye</h1>`;
+};
 
 module.exports = {
   'GET /hello/:name': fn_hello,
+  'GET /goodbye':fn_goodbye
 };
