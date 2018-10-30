54706e483dc670f069c11e1f9786e7bf1dcaebc2
zhp <354416479@qq.com>
2018-10-30 12:04:52 +0800

    no cors

--- controllers/bingEveryday.js
+++ controllers/bingEveryday.js
@@ -7,28 +7,9 @@ var fn_Pic = async (ctx, next) => {
     if (statusCode !== 200) {
       error = new Error('Request Failed.\n' +
         `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-        `Expected application/json but received ${contentType}`);
+    }else{
+      console.log(res);
     }
-    if (error) {
-      console.error(error.message);
-      // consume response data to free up memory
-      res.resume();
-      return;
-    }
-
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
   })
 };
 
