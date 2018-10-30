f906d3bf025c29944012b8fe81085cadbf6d1bef
zhp <354416479@qq.com>
2018-10-30 11:58:57 +0800

    no

---
+++ controllers/bingEveryday.js
@@ -0,0 +1,38 @@
+var http = require("http");
+var fn_Pic = async (ctx, next) => {
+  http.get('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1501558320736&pid=hp', (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  })
+};
+
+module.exports = {
+  'GET /api/bingPic': fn_Pic,
+
+};
