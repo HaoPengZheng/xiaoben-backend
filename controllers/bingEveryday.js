11e6997d6fe6129a72230121db0ee702aa1e1e9b
zhp <354416479@qq.com>
2018-11-06 14:45:58 +0800

    添加bing图片

--- controllers/bingEveryday.js
+++ controllers/bingEveryday.js
@@ -1,16 +1,15 @@
-var http = require("http");
+var rp = require('request-promise');
 var fn_Pic = async (ctx, next) => {
-  http.get('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1501558320736&pid=hp', (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-        `Status Code: ${statusCode}`);
-    }else{
-      console.log(res);
+  let bingPic ={};
+  await rp('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1501558320736&pid=hp').then(
+    response=>{
+      bingPic =  response;
     }
-  })
+  )
+  console.log("2")
+  ctx.response.type ='application/json'
+  ctx.response.status = 200;
+  ctx.body = bingPic;
 };
 
 module.exports = {
