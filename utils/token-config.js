66cf19a1c178bda2b6b6aeee75d1f13496a54f93
zhp <354416479@qq.com>
2018-11-15 16:49:51 +0800

    update token-config

--- utils/token-config.js
+++ utils/token-config.js
@@ -1,6 +1,6 @@
 const token_config = {
   secretKey : 'xiaoben-token',
   expiresIn : '2h',
-  unlessPath:  [/\/api\/user/,/\/api\/login/,/\/api\/bingPic/]
+  unlessPath:  [/\/api\/user/,/\/api\/login/,/\/api\/bingPic/],
 }
 module.exports = token_config;
